293b0139c2e41bebf8c24ee969c83357c63aa8cd
gongshijun <2276195537@qq.com>
2018-01-16 14:15:24 +0800

    2018/1/16

--- jniL/HelloJNI.java
+++ jniL/HelloJNI.java
@@ -2,3 +2,10 @@ public class HelloJNI {
     static {
         System.loadLibrary("hello"); // Load native library at runtime hello.dll (Windows) or libhello.so (Unixes)
     }
+    private native void sayHello(); // Declare a native method sayHello() that receives nothing and returns void
+    
+    // Test Driver
+    public static void main(String[] args) {
+        new HelloJNI().sayHello(); // invoke the native method
+    }
+}
